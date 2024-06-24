58398f29d779e01679b7dddf8e25d2dcee8611aa
battye <administration@online-scrabble.com>
2024-06-24 20:16:28 +0800

    Store setup.sh

---
+++ setup.sh
@@ -0,0 +1,44 @@
+# setup.sh
+
+# Start MySQL
+sudo service mysql start
+
+# Start Apache
+sudo service apache2 start
+
+# Add SSH key
+echo "$SSH_KEY" > /home/vscode/.ssh/id_rsa && chmod 600 /home/vscode/.ssh/id_rsa
+
+# Create a MySQL user to use
+sudo mysql -u root<<EOFMYSQL
+    CREATE USER 'phpbb'@'localhost' IDENTIFIED BY 'phpbb'; 
+    GRANT ALL PRIVILEGES ON *.* TO 'phpbb'@'localhost' WITH GRANT OPTION;
+    CREATE DATABASE IF NOT EXISTS phpbb;
+EOFMYSQL
+
+# Download dependencies for the extension
+echo "Dependencies"
+composer install --no-interaction
+
+# Install phpBB
+echo "phpBB project"
+composer create-project --no-interaction phpbb/phpbb /workspaces/phpbb
+
+# Copy phpBB config
+echo "Copy phpBB config"
+sudo cp .devcontainer/resources/phpbb-config.yml /workspaces/phpbb/install/install-config.yml
+
+echo "Symlink extension"
+sudo rm -rf /var/www/html
+sudo ln -s /workspaces/phpbb /var/www/html
+sudo mkdir /workspaces/phpbb/ext/phpbb
+sudo ln -s ./ /workspaces/phpbb/ext/phpbb/titania
+
+# Need to fix name above to be the name of the ext ^^
+
+echo "phpBB CLI install"
+cd /workspaces/phpbb && composer install --no-interaction
+sudo php /workspaces/phpbb/install/phpbbcli.php install /workspaces/phpbb/install/install-config.yml
+sudo rm -rf /workspaces/phpbb/install 
+
+echo "Completed"
